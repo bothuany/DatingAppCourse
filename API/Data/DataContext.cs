dfc403440f15f447a5a8eaa5176c52ec83624177
Recep Batuhan Dikmen <rbdikmen@gmail.com>
2024-11-21 23:24:53 +0300

    Add messaging functionality with Message entity, DTOs, and repository

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,17 +1,40 @@
 using System;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class DataContext(DbContextOptions options) : DbContext(options)
+public class DataContext(DbContextOptions options) : IdentityDbContext
+    <
+    AppUser, AppRole, int,
+    IdentityUserClaim<int>,
+    AppUserRole,
+    IdentityUserLogin<int>,
+    IdentityRoleClaim<int>,
+    IdentityUserToken<int>
+    >(options)
 {
     public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<AppUser>()
+            .HasMany(user => user.UserRoles)
+            .WithOne(user => user.User)
+            .HasForeignKey(user => user.UserId)
+            .IsRequired();
+
+        modelBuilder.Entity<AppRole>()
+            .HasMany(user => user.UserRoles)
+            .WithOne(user => user.Role)
+            .HasForeignKey(user => user.RoleId)
+            .IsRequired();
 
         modelBuilder.Entity<UserLike>()
             .HasKey(key => new { key.SourceUserId, key.TargetUserId });
@@ -27,5 +50,15 @@ public class DataContext(DbContextOptions options) : DbContext(options)
             .WithMany(user => user.LikedByUsers)
             .HasForeignKey(userLike => userLike.TargetUserId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Message>()
+            .HasOne(message => message.Recipient)
+            .WithMany(user => user.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Message>()
+            .HasOne(message => message.Sender)
+            .WithMany(user => user.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,41 +1,96 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
-import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TabDirective, TabsetComponent, TabsModule } from 'ngx-bootstrap/tabs';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { TimeagoModule } from 'ngx-timeago';
 import { DatePipe } from '@angular/common';
+import { MemberMessagesComponent } from '../member-messages/member-messages.component';
+import { Message } from '../../_models/message';
+import { MessageService } from '../../_services/message.service';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe],
+  imports: [
+    TabsModule,
+    GalleryModule,
+    TimeagoModule,
+    DatePipe,
+    MemberMessagesComponent,
+  ],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css',
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs', { static: true }) memberTabs?: TabsetComponent;
   private membersService = inject(MembersService);
+  private messageService = inject(MessageService);
   private route = inject(ActivatedRoute);
 
-  member?: Member;
+  member: Member = {} as Member;
   images: GalleryItem[] = [];
+  activeTab?: TabDirective;
+  messages: Message[] = [];
 
   ngOnInit(): void {
-    this.loadMember();
-  }
+    this.route.data.subscribe((data) => {
+      this.member = data['member'];
+      this.member &&
+        this.member.photos.forEach((photo) => {
+          this.images.push(
+            new ImageItem({ src: photo?.url, thumb: photo?.url })
+          );
+        });
+    });
 
-  loadMember() {
-    const username = this.route.snapshot.paramMap.get('username');
-    if (!username) {
-      return;
-    }
-
-    this.membersService.getMember(username).subscribe((member) => {
-      this.member = member;
-      member.photos.forEach((photo) => {
-        this.images.push(new ImageItem({ src: photo?.url, thumb: photo?.url }));
-      });
+    this.route.queryParams.subscribe((params) => {
+      params['tab'] && this.selectTab(params['tab']);
     });
   }
+
+  onUpdateMessages(event: Message) {
+    this.messages.push(event);
+  }
+
+  selectTab(heading: string) {
+    if (this.memberTabs) {
+      const messageTab = this.memberTabs.tabs.find(
+        (t) => t.heading === heading
+      );
+      if (messageTab) {
+        messageTab.active = true;
+      }
+    }
+  }
+
+  onTabActivated(data: TabDirective) {
+    this.activeTab = data;
+    if (
+      this.activeTab.heading === 'Messages' &&
+      this.messages.length === 0 &&
+      this.member
+    ) {
+      this.messageService
+        .getMessageThread(this.member.username)
+        .subscribe((messages) => {
+          this.messages = messages;
+        });
+    }
+  }
+
+  // loadMember() {
+  //   const username = this.route.snapshot.paramMap.get('username');
+  //   if (!username) {
+  //     return;
+  //   }
+
+  //   this.membersService.getMember(username).subscribe((member) => {
+  //     this.member = member;
+  //     member.photos.forEach((photo) => {
+  //       this.images.push(new ImageItem({ src: photo?.url, thumb: photo?.url }));
+  //     });
+  //   });
+  // }
 }
